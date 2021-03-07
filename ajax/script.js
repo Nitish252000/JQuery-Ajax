e18bc39b3ae847d53db422986674bca27708e1ba
Nitish <nitishkumar25082000@gmail.com>
2021-03-07 23:21:35 +0530

    Handling Errors

--- ajax/script.js
+++ ajax/script.js
@@ -1,18 +1,21 @@
 function fetchRandomDogImage(){
-    //var xhrRequest = new XMLHttpRequest();
-    //xhrRequest.onload=function(){
-    //    console.log(xhrRequest.response);
-    //    var responseJSON=JSON.parse(xhrRequest.response);
-    //    var imageUrl=responseJSON.message;
-    //    $('#dog-image').attr('src',imageUrl);
-    //};
-    //xhrRequest.open('get','https://dog.ceo/api/breeds/image/random',true);
+    var xhrRequest = new XMLHttpRequest();
+    xhrRequest.onload=function(){
+        console.log(xhrRequest.response);
+        var responseJSON=JSON.parse(xhrRequest.response);
+        var imageUrl=responseJSON.message;
+        $('#dog-image').attr('src',imageUrl);
+    };
+    xhrRequest.onerror=function(){
+        console.log('Request Failed')
+    };
+    xhrRequest.open('get','https://dog.ceo/api/breeds/image/random',true);
 
-    //xhrRequest.send();
+    xhrRequest.send();
 
 //Ajax Request With JQuery
 
-    /*
+ /* 
     $.ajax({
         url:"https://dog.ceo/api/breeds/image/random",
         method:'GET',
@@ -21,17 +24,21 @@ function fetchRandomDogImage(){
             $('#dog-image').attr('src',imageUrl);
         }}
 
+    }).fail(function(){
+        console.log('error');
     });
-    */
+ */   
 
     //even simpler
 
     
-        $.get('https://dog.ceo/api/breeds/image/random',function(data){
+      /*  $.get('https://dog.ceo/api/breeds/image/random',function(data){
             var imageUrl=data.message;
             $('#dog-image').attr('src',imageUrl);
+        }).fail(function(xhr,textStatus,errorThrown){
+            console.log("Request Failed");
         });
-
+*/
     
 
 }
